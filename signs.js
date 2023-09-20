01b195fc6e0f1615db9549ffceebddcf7019c1fe
Jay-Kishn <76587504+Jay-Kishn@users.noreply.github.com>
2023-09-20 23:52:25 +0530

    combined file 1

---
+++ signs.js
@@ -0,0 +1,338 @@
+aSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+aSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+aSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+aSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+aSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+aSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+aSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+aSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+aSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+aSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+bSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+bSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+bSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+bSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+bSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+bSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+bSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+bSign.addCurl(fp.Finger.Ring, fp.FingerCurl.NoCurl, 1);
+bSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+bSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.NoCurl, 1);
+bSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+aSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+aSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+aSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+aSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+aSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+aSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+aSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+aSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+aSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+aSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+bSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+bSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+bSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+bSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+bSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+bSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+bSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+bSign.addCurl(fp.Finger.Ring, fp.FingerCurl.NoCurl, 1);
+bSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+bSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.NoCurl, 1);
+bSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+cSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+cSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+cSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+cSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+cSign.addCurl(fp.Finger.Middle, fp.FingerCurl.HalfCurl, 1);
+cSign.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalUpRight, 0.70);
+cSign.addCurl(fp.Finger.Ring, fp.FingerCurl.HalfCurl, 1);
+cSign.addDirection(fp.Finger.Ring, fp.FingerDirection.DiagonalUpRight, 0.70);
+cSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.HalfCurl, 1);
+cSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpRight, 0.70);
+
+
+dSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+dSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+dSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+dSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+dSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+dSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+dSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+dSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+dSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+dSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+eSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+eSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+eSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+eSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+eSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+eSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+eSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+eSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+eSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+eSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+fSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+fSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+fSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+fSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+fSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+fSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+fSign.addCurl(fp.Finger.Ring, fp.FingerCurl.NoCurl, 1);
+fSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+fSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.NoCurl, 1);
+fSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+gSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+gSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+gSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+gSign.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalRight, 0.70);
+gSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+gSign.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalUpRight, 0.70);
+gSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+gSign.addDirection(fp.Finger.Ring, fp.FingerDirection.HorizontalRight, 0.70);
+gSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+gSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.HorizontalRight, 0.70);
+
+
+hSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+hSign.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalRight, 0.70);
+hSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+hSign.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalRight, 0.70);
+hSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+hSign.addDirection(fp.Finger.Middle, fp.FingerDirection.HorizontalRight, 0.70);
+hSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+hSign.addDirection(fp.Finger.Ring, fp.FingerDirection.HorizontalRight, 0.70);
+hSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+hSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.HorizontalRight, 0.70);
+
+
+iSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+iSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+iSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+iSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+iSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+iSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+iSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+iSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+iSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.NoCurl, 1);
+iSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+jSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+jSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+jSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+jSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+jSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+jSign.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalUpRight, 0.70);
+jSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+jSign.addDirection(fp.Finger.Ring, fp.FingerDirection.HorizontalRight, 0.70);
+jSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.NoCurl, 1);
+jSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.HorizontalRight, 0.70);
+
+
+kSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+kSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+kSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+kSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+kSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+kSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+kSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+kSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+kSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+kSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+lSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+lSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+lSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+lSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+lSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+lSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+lSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+lSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+lSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+lSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+mSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+mSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+mSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+mSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+mSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+mSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+mSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+mSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+mSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+mSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+nSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+nSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+nSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+nSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+nSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+nSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+nSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+nSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+nSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+nSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 0.70);
+
+
+oSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+oSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+oSign.addCurl(fp.Finger.Index, fp.FingerCurl.HalfCurl, 1);
+oSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+oSign.addCurl(fp.Finger.Middle, fp.FingerCurl.HalfCurl, 1);
+oSign.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalUpRight, 0.70);
+oSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+oSign.addDirection(fp.Finger.Ring, fp.FingerDirection.DiagonalUpRight, 0.70);
+oSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+oSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpRight, 0.70);
+
+
+pSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+pSign.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalRight, 0.70);
+pSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+pSign.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalRight, 0.70);
+pSign.addCurl(fp.Finger.Middle, fp.FingerCurl.HalfCurl, 1);
+pSign.addDirection(fp.Finger.Middle, fp.FingerDirection.DiagonalDownRight, 0.70);
+pSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+pSign.addDirection(fp.Finger.Ring, fp.FingerDirection.DiagonalDownRight, 0.70);
+pSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+pSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalDownRight, 0.70);
+
+
+qSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+qSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalDownRight, 0.70);
+qSign.addCurl(fp.Finger.Index, fp.FingerCurl.HalfCurl, 1);
+qSign.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalRight, 0.70);
+qSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+qSign.addDirection(fp.Finger.Middle, fp.FingerDirection.HorizontalRight, 0.70);
+qSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+qSign.addDirection(fp.Finger.Ring, fp.FingerDirection.DiagonalDownRight, 0.70);
+qSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+qSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalDownRight, 0.70);
+
+
+rSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+rSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+rSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+rSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+rSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+rSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+rSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+rSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+rSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+rSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+
+sSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+sSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+sSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+sSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+sSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+sSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+sSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+sSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+sSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+sSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 0.70);
+
+
+tSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+tSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+tSign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+tSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+tSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+tSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+tSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+tSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+tSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+tSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 0.70);
+
+
+uSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+uSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+uSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+uSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+uSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+uSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+uSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+uSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+uSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+uSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 0.70);
+
+
+vSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+vSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+vSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+vSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+vSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+vSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+vSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+vSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+vSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+vSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 0.70);
+
+
+wSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+wSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+wSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+wSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+wSign.addCurl(fp.Finger.Middle, fp.FingerCurl.NoCurl, 1);
+wSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+wSign.addCurl(fp.Finger.Ring, fp.FingerCurl.NoCurl, 1);
+wSign.addDirection(fp.Finger.Ring, fp.FingerDirection.DiagonalUpLeft, 0.70);
+wSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+wSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 0.70);
+
+
+xSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.HalfCurl, 1.0);
+xSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+xSign.addCurl(fp.Finger.Index, fp.FingerCurl.HalfCurl, 1);
+xSign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+xSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+xSign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+xSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+xSign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+xSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+xSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.VerticalUp, 0.70);
+
+
+ySign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+ySign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpRight, 0.70);
+ySign.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1);
+ySign.addDirection(fp.Finger.Index, fp.FingerDirection.VerticalUp, 0.70);
+ySign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+ySign.addDirection(fp.Finger.Middle, fp.FingerDirection.VerticalUp, 0.70);
+ySign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+ySign.addDirection(fp.Finger.Ring, fp.FingerDirection.VerticalUp, 0.70);
+ySign.addCurl(fp.Finger.Pinky, fp.FingerCurl.NoCurl, 1);
+ySign.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 0.70);
+
+
+zSign.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 0.8);
+zSign.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalLeft, 0.70);
+zSign.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
+zSign.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalUpLeft, 0.70);
+zSign.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1);
+zSign.addDirection(fp.Finger.Middle, fp.FingerDirection.HorizontalLeft, 0.70);
+zSign.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1);
+zSign.addDirection(fp.Finger.Ring, fp.FingerDirection.HorizontalLeft, 0.70);
+zSign.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1);
+zSign.addDirection(fp.Finger.Pinky, fp.FingerDirection.HorizontalLeft, 0.70);
+
